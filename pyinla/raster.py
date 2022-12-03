4fc18f2ae84582204a8b4d25c460e51e1d5030b4
Jeff Shen <shenjeff@princeton.edu>
2022-12-03 02:02:39 -0500

    add functions for dealing with raster maps

---
+++ pyinla/raster.py
@@ -0,0 +1,28 @@
+from pyinla.utils import ro
+from rpy2.robjects.packages import importr
+from pyinla.convert import R_NULL
+from typing import Optional
+
+ro.r("library(raster)")
+libraster = importr("raster")
+raster_extract = ro.r("raster::extract")
+
+
+def raster_to_points(raster):
+    return libraster.rasterToPoints(raster)
+
+
+def raster_aggregate(
+    raster,
+    fact: int,
+    fun: str = "mean",
+    expand: bool = False,
+    na_rm: bool = True,
+    by: Optional[str | int] = None,
+):
+    if by is None:
+        by = R_NULL
+
+    return ro.r("raster::aggregate")(
+        raster, fact, ro.r(fun), expand=expand, na_rm=na_rm, by=by
+    )
